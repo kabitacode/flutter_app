0a2f6308339f1922fe4122e3ebe9667c43fa640b
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-11 10:38:54 +0700

    ✨

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/providers/auth_provider.dart';
 import 'package:flutter_app/screen/main/main_screen.dart';
+import 'package:flutter_app/utils/theme.dart';
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -57,49 +58,65 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sign In')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  labelText: "Email"),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          isVisibility = !isVisibility;
-                        });
-                      },
-                      icon: Icon(isVisibility
-                          ? Icons.visibility
-                          : Icons.visibility_off)),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  labelText: "Password"),
-              obscureText: !isVisibility,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            _isloading
-                ? CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: () => _login(context), child: Text("Login")),
-            TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/register');
-                },
-                child: Text("Sign Up"))
-          ],
+      // appBar: AppBar(title: Text('Sign In')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 2, color: colorPrimary)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    labelText: "Email"),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 2, color: colorPrimary)),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isVisibility = !isVisibility;
+                          });
+                        },
+                        icon: Icon(isVisibility
+                            ? Icons.visibility
+                            : Icons.visibility_off)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    labelText: "Password"),
+                obscureText: !isVisibility,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _isloading
+                  ? CircularProgressIndicator()
+                  : Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(45),
+                              backgroundColor: colorPrimary),
+                          onPressed: () => _login(context),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+              // TextButton(
+              //     onPressed: () {
+              //       Navigator.pushNamed(context, '/register');
+              //     },
+              //     child: Text("Sign Up"))
+            ],
+          ),
         ),
       ),
     );
