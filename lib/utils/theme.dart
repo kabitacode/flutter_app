8904064c39384d38285699b0d1a8bd37ebe00c6d
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-11 13:46:26 +0700

    ✨

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color colorPrimary = Color(0xFF9DBDFF);
+const Color colorSecondary = Color(0xFF7695FF);
+const Color colorOrange = Color(0xFFFF9874);
+const Color colorYellow = Color(0xFFFABC3F);
+
+const Color colorTypography = Color(0xFF1E201E);
