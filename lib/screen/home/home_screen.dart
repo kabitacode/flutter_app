1dc7851e803f799e97d8cbdd5765f50f635a3b81
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-12 15:31:38 +0700

    add icon

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,21 +1,130 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/providers/auth_provider.dart';
-import 'package:flutter_app/providers/splash_provider.dart';
+import 'package:flutter_app/utils/theme.dart';
+import 'package:flutter_app/widgets/custom_button_icon.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController searchController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    final token = Provider.of<SplashProvider>(context);
+    final token = Provider.of<AuthProvider>(context);
 
     return Scaffold(
-      body: Center(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.camera_alt_outlined),
+                  ),
+                  Expanded(
+                      child: Container(
+                    height: 45,
+                    child: TextField(
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(Icons.search),
+                        labelText: "Search..",
+                        border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: colorTypography, width: 2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 15, color: colorTypography),
+                    ),
+                  )),
+                ],
+              ),
+            ),
+            Wrap(
+              spacing: 20,
+              runSpacing: 20,
+              alignment: WrapAlignment.spaceAround,
+              children: [
+                CustomButtonIcon(
+                    icon: Icons.discount,
+                    colorButton: colorOrange,
+                    onTap: () {},
+                    label: "Discount"),
+                CustomButtonIcon(
+                    icon: Icons.broadcast_on_personal_outlined,
+                    colorButton: colorPrimary,
+                    onTap: () {},
+                    label: "Credit"),
+                CustomButtonIcon(
+                    icon: Icons.payment,
+                    colorButton: colorSecondary,
+                    onTap: () {},
+                    label: "Payment"),
+                CustomButtonIcon(
+                    icon: Icons.phone_android_sharp,
+                    colorButton: colorOther,
+                    onTap: () {},
+                    label: "TopUp"),
+                CustomButtonIcon(
+                    icon: Icons.payments_rounded,
+                    colorButton: colorOther_1,
+                    onTap: () {},
+                    label: "Voucher"),
+                CustomButtonIcon(
+                    icon: Icons.paypal_outlined,
+                    colorButton: colorOther_2,
+                    onTap: () {},
+                    label: "Bill"),
+                CustomButtonIcon(
+                    icon: Icons.monetization_on_outlined,
+                    colorButton: colorPurple,
+                    onTap: () {},
+                    label: "Claim"),
+                CustomButtonIcon(
+                    icon: Icons.games,
+                    colorButton: colorOther_3,
+                    onTap: () {},
+                    label: "Games"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UncontainedLayoutCard extends StatelessWidget {
+  const UncontainedLayoutCard({
+    super.key,
+    required this.index,
+    required this.label,
+  });
+
+  final int index;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.primaries[index % Colors.primaries.length].withOpacity(0.5),
+      child: Center(
         child: Text(
-          'Token: ${token.token ?? "Tidak ada token"}',
-          style: const TextStyle(fontSize: 16),
-          textAlign: TextAlign.center,
+          label,
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+          overflow: TextOverflow.clip,
+          softWrap: false,
         ),
       ),
     );
