844f95795815e38e728382cd1d7907cee07e4d53
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-12 15:31:45 +0700

    add color more

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -4,5 +4,10 @@ const Color colorPrimary = Color(0xFF9DBDFF);
 const Color colorSecondary = Color(0xFF7695FF);
 const Color colorOrange = Color(0xFFFF9874);
 const Color colorYellow = Color(0xFFFABC3F);
+const Color colorPurple = Color(0xFF624E88);
+const Color colorOther = Color(0xFFC96868);
+const Color colorOther_1 = Color(0xFF7EACB5);
+const Color colorOther_2 = Color(0xFFF4DEB3);
+const Color colorOther_3 = Color(0xFFF7EFE5);
 
 const Color colorTypography = Color(0xFF1E201E);
