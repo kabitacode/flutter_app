749c423f9b649cabd57f73aeecfec6e08b1a4f29
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-12 15:07:30 +0700

    ✨ add custom button

---
+++ lib/widgets/custom_button_icon.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/utils/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomButtonIcon extends StatelessWidget {
+  final Color colorButton;
+  final IconData icon;
+  final VoidCallback onTap;
+  final String label;
+
+  const CustomButtonIcon(
+      {Key? key,
+      required this.icon,
+      required this.colorButton,
+      required this.onTap,
+      required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: colorButton, borderRadius: BorderRadius.circular(10)),
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+        child: Column(
+          children: [
+            Icon(Icons.discount),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Discount",
+              style: GoogleFonts.lato(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  color: colorTypography),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
