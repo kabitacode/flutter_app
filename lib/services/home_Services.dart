ed5b5ab450b260d2e2235571857fa42e3f3b5778
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-17 15:06:05 +0700

    add services home

--- lib/services/home_Services.dart
+++ lib/services/home_Services.dart
@@ -1,17 +1,21 @@
 import 'dart:convert';
 
+import 'package:flutter_app/models/product.dart';
 import 'package:http/http.dart' as http;
 
 class HomeServices {
   final String baseUrl = "https://api.escuelajs.co/api/v1/products";
-  Future<void> getProduct() async {
+
+  Future<List<Product>> getProduct() async {
     try {
       final res = await http.get(Uri.parse(baseUrl),
           headers: {'Content-Type': 'application/json'});
 
       if (res.statusCode == 200) {
-        final result = json.decode(res.body);
-        return result;
+        final List<dynamic> result = json.decode(res.body);
+        return result.map((item) => Product.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to fetch product');
       }
     } catch (e) {
       throw Exception('Failed to fetch : $e');
